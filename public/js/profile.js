7256e9b9639517eef5c0520bf32927b41749940f
JordAnne <jbethune37@gmail.com>
2023-04-06 19:55:22 -0400

    edited handlebar, html, script file

---
+++ public/js/profile.js
@@ -0,0 +1,46 @@
+const newPostHandler = async (event) => {
+    event.preventDefault();
+      
+    const postCreate = document.querySelector('#new-post').value.trim();
+    
+        if (postCreate) {
+          const response = await fetch(``, {
+            method: 'POST',
+            body: JSON.stringify({postCreate}),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+      
+          if (response.ok) {
+            document.location.replace('/profile');
+          } else {
+            alert('Failed to create post');
+          }
+        }
+      };
+      
+      const delButtonHandler = async (event) => {
+        if (event.target.hasAttribute('data-id')) {
+          const id = event.target.getAttribute('data-id');
+      
+          const response = await fetch(`/api/projects/${id}`, {
+            method: 'DELETE',
+          });
+      
+          if (response.ok) {
+            document.location.replace('/profile');
+          } else {
+            alert('Failed to delete project');
+          }
+        }
+      };
+      
+      document
+        .querySelector('.new-project-form')
+        .addEventListener('submit', newFormHandler);
+      
+      document
+        .querySelector('.project-list')
+        .addEventListener('click', delButtonHandler);
+    
