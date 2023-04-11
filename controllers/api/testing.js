e9a9b8a65139a7372868360a2e08b6dfd0a32696
00rest <119826790+00rest@users.noreply.github.com>
2023-04-11 19:46:42 -0400

    soeme comments

--- controllers/api/testing.js
+++ controllers/api/testing.js
@@ -2,54 +2,7 @@ const router = require('express').Router();
 const { Post, User} = require('../../models');
 //const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-      // Get all projects and JOIN with user data
-      const postData = await Post.findAll();
-        // Serialize data so the template can read it
-      const posts = postData.map((post) => post.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('profile', {posts});
-        // console.log('||||- ! -||||', posts);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
 
 
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const newProject = await Project.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newProject);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No project found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
