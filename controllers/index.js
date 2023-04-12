c553f0f110d6128761f260c4c4b56a25eb721cd1
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-04-11 20:55:29 -0400

    fixed login routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-//const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoute');
 
-//router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
